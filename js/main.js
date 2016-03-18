90e7b9335ddff39e8d8d364ea0d73305e597babd
Kyle-Jackson <kylejackson@Kyles-MacBook-Pro.local>
2016-03-18 10:11:49 -0400

    Fixed Errors.

--- js/main.js
+++ js/main.js
@@ -1,35 +1,9 @@
-var $body = $('body');
+var $ball = $('.ball');
+var $input = $('#color');
 
-$('html').on('keydown', function (e) {
-  var $newBall = $('<div>');
-  var newLeft = Math.floor(Math.random() * (1000 - 0)) + 0;
-  var newTop = Math.floor(Math.random() * (1000 - 0)) + 0;
+$('form').on('change', function (e) {
+  var ballColor = $input.val();
 
-  $newBall.addClass('ball');
-  $body.append($newBall);
-  $newBall.css('left', newLeft);
-  $newBall.css('top', newTop);
-
-  var $ball = $('.ball');
-
-  $('#btn-right') .on('click', function () {
-    var newLeft = $ball.offset() .left + 10;
-    $ball.css('left', newLeft);
-  });
-
-  $('#btn-left') .on('click', function () {
-    var newLeft = $ball.offset() .left - 10;
-    $ball.css('left', newLeft);
-  })
-
-  $('#btn-up') .on('click', function () {
-    var newTop = $ball.offset() .top - 10;
-    $ball.css('top', newTop);
-  });
-
-  $('#btn-down') .on('click', function () {
-    var newTop = $ball.offset() .top + 10;
-    $ball.css('top', newTop);
-  });
+  $ball.css('background-color', ballColor);
 
 });
